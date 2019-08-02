9c0c09ddd7966ed4b64c18e9b2fa61e71e1dc349
Terrasse <3305049949@qq.com>
2019-08-02 21:06:22 +0800

    Beautify

--- server.js
+++ server.js
@@ -5,8 +5,8 @@
 	var URL = require('url');
 	var server = http.createServer();
  
-	server.listen(9001, function(){
-		 console.log('RUN:9001');
+	server.listen(7777, function(){
+		 console.log('RUN:7777');
 	})
 	
 	
@@ -26,7 +26,7 @@
 		
 		}else if(par.pathname === '/submit'){
             text = decodeURI(par.query)
-            fs.appendFile('./log.txt','<p>' + text + '</p>\n', (error)  => {
+            fs.appendFile('./log.txt','<div class="mdui-row"><div class="mdui-card mdui-col-xs-2"><div class="mdui-card-content">' + text + '</div></div></div>\n', (error)  => {
                 if (error) return console.log("Fail: " + error.message);
                 console.log("Received: " + text);
             });
@@ -37,7 +37,38 @@
 				}
 				response.end(data);
 			});
-		}else{
+        } else if(par.pathname === '/clear') {
+            fs.writeFile('./log.txt', '',{},function(err){
+                if(err){
+                    throw err;
+                }
+                console.log("Cleared History");
+            });
+            response.writeHead(200,{'Content-Type':'text/html;charset=UTF-8'});
+			fs.readFile('./notFount.html','utf-8',function(err,data){
+				if(err){
+					throw err ;
+				}
+				response.end(data);
+			});
+        } else {
+            path = par.pathname;
+            type = path.split('/')[1];
+            if(type != 'css') {
+                if(type == 'js') {
+                    type = 'javascript';
+                } else {
+                    type = 'plain';
+                }
+            }
+            response.writeHead(200, {'Content-Type': 'text/' + type + ';charset=UTF-8'});
+            fs.readFile('.' + path, 'utf-8', function(err, data) {
+                if(err){
+                    throw err;
+                }
+                response.end(data);
+            });
+		}/*else{
 			response.writeHead(200,{'Content-Type':'text/html;charset=UTF-8'});
 			fs.readFile('./notFount.html','utf-8',function(err,data){
 				if(err){
@@ -45,6 +76,6 @@
 				}
 				response.end(data);
 			});
-		}
+		}*/
 	    console.log("once");	
 	});
